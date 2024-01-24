c281e12d86411c8e0a81ffa71b77513dfd5b6f07
Komal <140838139+komal-sp@users.noreply.github.com>
2024-01-24 11:57:44 +0530

    Update agent tools.py

--- bootcamp/Agents/agent tools.py
+++ bootcamp/Agents/agent tools.py
@@ -2,5 +2,5 @@ import os
 from langchain.agents import load_tools, initialize_agent, AgentType
 from langchain.chat_models import ChatOpenAI
 
-OPENAI_API_KEY = os.environ['OPENAI_API_KEY'] = "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"
+OPENAI_API_KEY = os.environ['OPENAI_API_KEY'] = ""
 llm = ChatOpenAI(openai_api_key=OPENAI_API_KEY, temperature=0)
