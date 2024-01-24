e49526ac3479f3b8c15b9d9eb9299d7d7fd6ea14
Komal <140838139+komal-sp@users.noreply.github.com>
2024-01-24 11:56:46 +0530

    Update doc html loader.py

--- bootcamp/Data Connections/doc html loader.py
+++ bootcamp/Data Connections/doc html loader.py
@@ -1,7 +1,7 @@
 import os
 from langchain.document_loaders import BSHTMLLoader
 
-OPENAI_API_KEY = os.environ['OPENAI_API_KEY'] = "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"
+OPENAI_API_KEY = os.environ['OPENAI_API_KEY'] = ""
 loader = BSHTMLLoader("../moduleIO/some_data/some_website.html")
 data = loader.load()
 # print(data)
