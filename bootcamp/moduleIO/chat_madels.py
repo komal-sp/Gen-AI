c9416b554c27c127b72cb9717083578af600e061
Komal <140838139+komal-sp@users.noreply.github.com>
2024-01-24 11:53:56 +0530

    Update chat_madels.py

--- bootcamp/moduleIO/chat_madels.py
+++ bootcamp/moduleIO/chat_madels.py
@@ -2,7 +2,7 @@ from langchain.chat_models import ChatOpenAI
 import os
 from langchain.schema import AIMessage, HumanMessage, SystemMessage
 
-OPENAI_API_KEY = os.environ['OPENAI_API_KEY'] = "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"
+OPENAI_API_KEY = os.environ['OPENAI_API_KEY'] = ""
 chat = ChatOpenAI(openai_api_key=OPENAI_API_KEY)
 
 result = chat([SystemMessage(content="You are   very rude lazy teenager who only wants to party and not answer the questions"),
