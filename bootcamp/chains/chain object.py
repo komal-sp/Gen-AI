59cafc150c736a637759327bd656a0abeb88beb0
Komal <140838139+komal-sp@users.noreply.github.com>
2024-01-24 11:55:08 +0530

    Update chain object.py

--- bootcamp/chains/chain object.py
+++ bootcamp/chains/chain object.py
@@ -3,7 +3,7 @@ from langchain.chat_models import ChatOpenAI
 from langchain.prompts.chat import ChatPromptTemplate, HumanMessagePromptTemplate
 from langchain.chains import LLMChain
 
-OPENAI_API_KEY = os.environ['OPENAI_API_KEY'] = "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"
+OPENAI_API_KEY = os.environ['OPENAI_API_KEY'] = ""
 
 human_template = "Make up a funny company name for a company that makes: {product}"
 human_prompt = HumanMessagePromptTemplate.from_template(human_template)
